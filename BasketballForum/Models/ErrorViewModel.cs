9b20506ba53a3fadcd65bfbfed25bb44bbe27482
Sanchen Zhang <zsckevin777@gmail.com>
2025-01-21 14:39:07 -0400

    Add project files.

---
+++ BasketballForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BasketballForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
